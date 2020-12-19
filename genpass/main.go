a6f12f5897d2c7dd9e1e0542a9f566b899b73b32
Francisco <fcootz@outlook.com>
2020-12-19 13:53:31 -0700

    genpass finished

--- genpass/main.go
+++ genpass/main.go
@@ -4,6 +4,7 @@ import (
 	"crypto/rand"
 	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
@@ -13,9 +14,29 @@ func main() {
 
 	flag.Parse()
 
-	fmt.Println("Website: " + *website)
-	fmt.Println("Username: " + *username)
-	fmt.Println("Password: " + randPass(*size))
+	homePath, err := os.UserHomeDir()
+	check(err)
+
+	pass_file, err := os.OpenFile(homePath+"/.passwords.txt", os.O_APPEND|os.O_WRONLY, 0644)
+	check(err)
+
+	web := "Website: " + *website
+	usr := "Username: " + *username
+	psw := "Password: " + randPass(*size)
+	nl := "\n"
+
+	d := []string{web, usr, psw, nl}
+
+	for _, v := range d {
+		fmt.Fprintln(pass_file, v)
+		check(err)
+	}
+
+	err = pass_file.Close()
+	check(err)
+	fmt.Println(web)
+	fmt.Println(usr)
+	fmt.Println(psw)
 
 }
 
@@ -31,9 +52,9 @@ func GenerateRandomBytes(n int) ([]byte, error) {
 func randPass(size int) string {
 	var password string
 	password = `(){}[]<>/+=-_,.~€#!@£$%^&*?|0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`
-	var bytes = make([]byte, size)
-	//var bytes, err = GenerateRandomBytes(size)
-	//check(err)
+	//var bytes = make([]byte, size)
+	var bytes, err = GenerateRandomBytes(size)
+	check(err)
 
 	rand.Read(bytes)
 	for k, v := range bytes {
